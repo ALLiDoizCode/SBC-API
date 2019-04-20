b86275deb0ae3549934ab799cb48ba9e204ffb60
Di3twater <dev.jonathan.green@gmail.com>
2019-04-20 15:06:39 -0400

    WIP

--- Sources/App/Models/ClientRoute.swift
+++ Sources/App/Models/ClientRoute.swift
@@ -3,27 +3,45 @@ import Crypto
 import Foundation
 enum ClientRoute {
     
-    case submitRCL(tx:TX)
+    case submitRCL(blockchain:XRP)
     case submitETH(blockchain:ETH)
     case submitBTC(tx:String)
+
     func request() throws -> HTTPRequest {
         var httpReq = HTTPRequest()
-        let encoder = JSONEncoder()
         switch self {
             
-            case .submitRCL(let tx):
+            case .submitRCL(let blockchain):
                 httpReq = HTTPRequest(method: .POST, url: RCL_URL_TEST)
-                let data = try encoder.encode(tx)
-                httpReq.body = HTTPBody(data: data)
+                httpReq.body = HTTPBody(data: try blockchain.data())
                 return httpReq
             case .submitETH(let blockchain):
-                httpReq = HTTPRequest(method: .POST, url: "ETH_URL_Test\(blockchain.endpoint)") 
+                httpReq = HTTPRequest(method: .POST, url: "ETH_URL_Test\(try blockchain.endpoint())") 
                 return httpReq       
             case .submitBTC(let tx):
                 httpReq = HTTPRequest(method: .POST, url: BTC_URL_TEST)
                 return httpReq    
         }
         
+    }
+}
+
+enum XRP {
+    case submitXRP(blob:String)
+    case accountInfo(address:String)
+
+    func data() throws -> Data {
+        let encoder = JSONEncoder()
+        switch self {
+            case .submitXRP(let blob):
+                let tx_blob = TX(tx_blob:blob)
+                let data = try encoder.encode(tx_blob)
+                return data
+            case .accountInfo(let address):
+                let account_info = Account_Info(address:address)
+                let data = try encoder.encode(account_info)
+                return data
+        }
     }
 }
 
@@ -34,13 +52,13 @@ enum ETH {
 
     func endpoint() throws -> String {
         switch self {
-        case .submitETH(let blob):
-            return "module=proxy&action=eth_sendRawTransaction&hex=\(blob)&apikey=\(ETH_API_TOKEN)"
-        case .balance(let address):
-            return "module=account&action=balance&address=\(address)&tag=latest&apikey=\(ETH_API_TOKEN)"
-        case .transactionCount(let address):
-            return "module=proxy&action=eth_getTransactionCount&address=\(address)&tag=latest&apikey=\(ETH_API_TOKEN)"  
-    }
+            case .submitETH(let blob):
+                return "module=proxy&action=eth_sendRawTransaction&hex=\(blob)&apikey=\(ETH_API_TOKEN)"
+            case .balance(let address):
+                return "module=account&action=balance&address=\(address)&tag=latest&apikey=\(ETH_API_TOKEN)"
+            case .transactionCount(let address):
+                return "module=proxy&action=eth_getTransactionCount&address=\(address)&tag=latest&apikey=\(ETH_API_TOKEN)"  
+        }
     }
 }
 
--- Sources/App/Models/RCLModels.swift
+++ Sources/App/Models/RCLModels.swift
@@ -2,21 +2,37 @@ import Vapor
 
 struct TX:Content {
     var method = "submit"
-    var params:[Blob]
+    var params:[Blob] = []
+    init(tx_blob:String) {
+        let blob = Blob(tx_blob:tx_blob)
+        params.append(blob)
+    }
 }
 
 struct Blob:Content {
     var tx_blob = ""
+    init(tx_blob:String) {
+        self.tx_blob = tx_blob
+    }
 } 
 
 struct Account_Info:Content {
-    var method = "submit"
-    var params:[Account]
+    var method = "account_info"
+    var params:[Account] = []
+
+    init(address:String) {
+        let account = Account(address:address)
+        params.append(account)
+    }
 }
 
 struct Account:Content {
     var account = ""
     var ledger_index = "validated"
+
+    init(address:String) {
+        self.account = address
+    }
 }
 
 
